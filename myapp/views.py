aa5fb400c73594b103a7eb7836ec306bfe2029d1
Yuxi <liuyuxi.yx@gmail.com>
2024-08-25 00:40:47 +1000

    Add basics of game

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def game(request):
+    room = get_object_or_404(Room, room_id='0a9f4e42-6e49-44b6-95f3-b1b876491038')
+    players = room.room_players.all()
+    texts = room.room_texts.all()
+    return render(request, 'game.html', {'room': room, 'players': players, 'texts': texts})
