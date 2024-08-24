c976b4d349c58fc527fe822e305b592e0918f33a
Yuxi <liuyuxi.yx@gmail.com>
2024-08-25 00:49:43 +1000

    Set up game

--- myapp/models.py
+++ myapp/models.py
@@ -13,7 +13,7 @@ class Room(models.Model):
         return f"Room {self.room_id} hosted by {self.host}"
     
 class Player(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name="room_players", on_delete=models.CASCADE)
     user = models.CharField(max_length=50)
     score = models.PositiveIntegerField(default=0)
 
@@ -21,9 +21,10 @@ class Player(models.Model):
         return f"{self.user} in Room {self.room.room_id}"
 
 class Sentence(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name="room_texts", on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, related_name="player_texts", on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
-        return f"Sentence by {self.player.user}"
+        return f"Sentence by {self.player.user} in {self.room.room_id}"
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def game(request):
+    room = get_object_or_404(Room, room_id='703acb23-d420-4524-a053-03a1132b98ed')
+    players = room.room_players.all()
+    texts = room.room_texts.all()
+    return render(request, 'game.html', {'room': room, 'players': players, 'texts': texts})
